7f0026c7980b96465cade97a9260f915d50a0d3c
luishardstyle <l.valdescastro15@gmail.com>
2023-01-18 17:13:39 -0300

    add TodoContext-localStorage and hook

---
+++ src/App/AppUI.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { TodoCounter } from '../components/TodoCounter/TodoCounter';
+import { TodoList } from '../components/TodoList/TodoList';
+import { TodoSearch } from '../components/TodoSearch/TodoSearch';
+import { TodoItem } from '../components/TodoItem/TodoItem';
+import { CreateTodoButton } from '../components/CreateTodoButton/CreateTodoButton';
+import { TodoContext } from '../TodoContext';
+
+
+function AppUI() {
+    const {
+      error,
+      loading,
+      searchedTodos,
+      completeTodo,
+      deleteTodo,
+    } = React.useContext(TodoContext);
+    
+    return (
+      <React.Fragment>
+        <TodoCounter />
+        <TodoSearch />
+  
+        <TodoList>
+          {error && <p>Desespérate, hubo un error...</p>}
+          {loading && <p>Estamos cargando, no desesperes...</p>}
+          {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+          
+          {searchedTodos.map(todo => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+        </TodoList>
+  
+  
+        <CreateTodoButton />
+      </React.Fragment>
+    );
+  }
+  
+  export { AppUI };
--- src/App/index.js
+++ src/App/index.js
@@ -1,80 +1,14 @@
-import { useState } from "react";
-import { CreateTodoButton } from "../components/CreateTodoButton/CreateTodoButton";
-import { TodoCounter } from "../components/TodoCounter/TodoCounter";
-import { TodoItem } from "../components/TodoItem/TodoItem";
-import { TodoList } from "../components/TodoList/TodoList";
-import { TodoSearch } from "../components/TodoSearch/TodoSearch";
+import React from 'react';
+import { TodoProvider } from '../TodoContext';
+import { AppUI } from './AppUI';
 
-
-const defaultTodos = [
-  { text: 'Cortar Cebolla', completed: false},
-  { text: 'tomar un curso', completed: false},
-  { text: 'llorar con la llorona', completed: false},
-]
 
 function App() {
-  const [todo, setTodo] = useState(defaultTodos);
-  const [searchValue, setSearchValue] = useState('');
-
-  const completedTodo = todo.filter(todo => !!todo.completed).length;
-  const totalTodos = todo.length;
-
-  let searchedTodos = [];
-
-  if(!searchValue.length >= 1){
-    searchedTodos = todo;
-  }else{
-    searchedTodos = todo.filter(todo =>{
-      const todoText = todo.text.toLowerCase()
-      const searchText = searchValue.toLowerCase()
-      return todoText.includes(searchText);
-    })
-    
-  }
-
-  const completeTodo = (text) =>{
-    const todoIndex = todo.findIndex(todo => todo.text === text);
-
-    const newTodos = [...todo];
-    todo[todoIndex].completed = true;
-    setTodo(newTodos);
-    
-  }
-
-  const deleteTodo = (text) =>{
-    const todoIndex = todo.findIndex(todo => todo.text === text);
-
-    const newTodos = [...todo];
-    newTodos.splice(todoIndex, 1);
-    setTodo(newTodos);
-    
-  }
-
   return (
-    <>
-    <TodoCounter
-    total={totalTodos}
-    completed= {completedTodo}
-    />
-    <TodoSearch
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-    />
-
-    <TodoList>
-      {searchedTodos.map(todo =>(
-        <TodoItem 
-        key= {todo.text} 
-        text={todo.text} 
-        completed={todo.completed} 
-        onComplete = {()=> completeTodo(todo.text)}
-        onDelete = {()=>deleteTodo(todo.text)}
-        />
-        ))}
-    </TodoList>
-
-    <CreateTodoButton/>
-    </>
+    <TodoProvider>
+      <AppUI/>
+    </TodoProvider>
+   
   );
 }
 
